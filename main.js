62d638284ba54d8354fef1dbf5574d9cea4dfbf0
Bruno Santos <brunofrs7@gmail.com>
2024-04-16 15:26:37 +0100

    Alterado main.js

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 Novo ficheiro
+
+[master (root-commit) 7a239f5] Adicionados ficheiros index e style
+ 2 files changed, 9 insertions(+)
+ create mode 100644 index.html
+ create mode 100644 style.css
+
+ [master 5292192] Alterados ficheiros index e style; Adicionado ficheiro main.js
+ 3 files changed, 2 insertions(+), 1 deletion(-)
+ create mode 100644 main.js
